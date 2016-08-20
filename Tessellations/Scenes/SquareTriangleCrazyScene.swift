03ada0f0c7773c1cf3a38ef4667d5ee7e2710736
Jman012 <jman012guy@gmail.com>
2016-08-19 21:49:34 -0700

    Finally updated the Square Triangle craziness scene to use the Shapes. Still haven't done proper sizing and positioning.

--- Tessellations/Scenes/SquareTriangleCrazyScene.swift
+++ Tessellations/Scenes/SquareTriangleCrazyScene.swift
@@ -11,10 +11,13 @@ import SpriteKit
 
 class SquareTriangleCrazyScene: AbstractGameBoardScene {
 
-    var squareWidth: CGFloat = 0
-    var tri_a: CGFloat = 0
-    var tri_r: CGFloat = 0
-    var tri_R: CGFloat = 0
+    var square: Square!
+    var square30: Square30!
+    var triangleUp: TriangleUp!
+    var triangleDown: TriangleDown!
+    var triangleLeft: TriangleLeft!
+    var triangleRight: TriangleRight!
+    
     var totalHeight: CGFloat = 0
     
     override func initLogicalBoard() -> AbstractGameBoard {
@@ -25,75 +28,35 @@ class SquareTriangleCrazyScene: AbstractGameBoardScene {
     
     override func setShapePaths() {
         
-        squareWidth = 45.0
-        var squarePath = CGPathCreateMutable()
-        CGPathMoveToPoint(squarePath, nil, -squareWidth/2, -squareWidth/2)
-        CGPathAddLineToPoint(squarePath, nil, squareWidth/2, -squareWidth/2)
-        CGPathAddLineToPoint(squarePath, nil, squareWidth/2, squareWidth/2)
-        CGPathAddLineToPoint(squarePath, nil, -squareWidth/2, squareWidth/2)
-        CGPathCloseSubpath(squarePath)
-        var transformScale = CGAffineTransformMakeScale(0.95, 0.95)
-        squarePath = CGPathCreateMutableCopyByTransformingPath(squarePath, &transformScale)!
-        self.shapePaths[.Square] = squarePath
+        let squareWidth: CGFloat = 45.0
+        let pipeWidth = squareWidth / 3.0
         
-        var transformRotate = CGAffineTransformMakeRotation(CGFloat(30.0.degrees))
-        let square30Path = CGPathCreateMutableCopyByTransformingPath(squarePath, &transformRotate)
-        self.shapePaths[.Square30] = square30Path
+        square = Square(width: squareWidth, pipeWidth: pipeWidth)
+        self.shapePaths[.Square] = square.path
+        self.pipePaths[.Square] = square.pipePath
         
-        let pipeWidth = squareWidth / 4.0
-        var squarePipePath = CGPathCreateMutable()
-        CGPathMoveToPoint   (squarePipePath, nil, pipeWidth * (-1/2), squareWidth/2 - 0.5)
-        CGPathAddLineToPoint(squarePipePath, nil, pipeWidth * (1/2), squareWidth/2 - 0.5)
-        CGPathAddLineToPoint(squarePipePath, nil, pipeWidth * (1/2), 0)
-        CGPathAddArc        (squarePipePath, nil, 0, 0, pipeWidth * (1/2), 0, CGFloat(M_PI), true)
-        CGPathCloseSubpath  (squarePipePath)
-        transformScale = CGAffineTransformMakeScale(1.0, 0.95)
-        squarePipePath = CGPathCreateMutableCopyByTransformingPath(squarePipePath, &transformScale)!
-        self.pipePaths[.Square] = squarePipePath
+        square30 = Square30(width: squareWidth, pipeWidth: pipeWidth)
+        self.shapePaths[.Square30] = square30.path
+        self.pipePaths[.Square30] = square30.pipePath
         
-        self.pipePaths[.Square30] = squarePipePath
+        triangleUp = TriangleUp(sideLength: squareWidth, pipeWidth: pipeWidth)
+        self.shapePaths[.TriangleUp] = triangleUp.path
+        self.pipePaths[.TriangleUp] = triangleUp.pipePath
+        
+        triangleDown = TriangleDown(sideLength: squareWidth, pipeWidth: pipeWidth)
+        self.shapePaths[.TriangleDown] = triangleDown.path
+        self.pipePaths[.TriangleDown] = triangleDown.pipePath
+        
+        triangleLeft = TriangleLeft(sideLength: squareWidth, pipeWidth: pipeWidth)
+        self.shapePaths[.TriangleLeft] = triangleLeft.path
+        self.pipePaths[.TriangleLeft] = triangleLeft.pipePath
+        
+        triangleRight = TriangleRight(sideLength: squareWidth, pipeWidth: pipeWidth)
+        self.shapePaths[.TriangleRight] = triangleRight.path
+        self.pipePaths[.TriangleRight] = triangleRight.pipePath
+        
 
-        
-        tri_a = squareWidth
-        tri_r = tri_a * sqrt(3.0) / 6.0
-        tri_R = tri_a * sqrt(3.0) / 3.0
-        totalHeight = CGFloat(self.logicalBoardHeight) * (tri_r + tri_R)
-        
-        var triangleUpPath = CGPathCreateMutable()
-        var angle = 90.0
-        CGPathMoveToPoint(triangleUpPath, nil, tri_R * CGFloat(cos(angle.degrees)), tri_R * CGFloat(sin(angle.degrees)))
-        for _ in 0..<2 {
-            angle += 120.0
-            CGPathAddLineToPoint(triangleUpPath, nil, tri_R * CGFloat(cos(angle.degrees)), tri_R * CGFloat(sin(angle.degrees)))
-        }
-        CGPathCloseSubpath(triangleUpPath)
-        transformScale = CGAffineTransformMakeScale(0.95, 0.95)
-        triangleUpPath = CGPathCreateMutableCopyByTransformingPath(triangleUpPath, &transformScale)!
-        self.shapePaths[.TriangleUp] = triangleUpPath
-        
-        transformRotate = CGAffineTransformMakeRotation(CGFloat(90.0.degrees))
-        var trianglePath = CGPathCreateMutableCopyByTransformingPath(triangleUpPath, &transformRotate)!
-        self.shapePaths[.TriangleLeft] = trianglePath
-        
-        trianglePath = CGPathCreateMutableCopyByTransformingPath(trianglePath, &transformRotate)!
-        self.shapePaths[.TriangleDown] = trianglePath
-        
-        trianglePath = CGPathCreateMutableCopyByTransformingPath(trianglePath, &transformRotate)!
-        self.shapePaths[.TriangleRight] = trianglePath
-        
-        
-        var trianglePipePath = CGPathCreateMutable()
-        CGPathMoveToPoint   (trianglePipePath, nil, pipeWidth * (-1/2), tri_r - 0.5)
-        CGPathAddLineToPoint(trianglePipePath, nil, pipeWidth * (1/2), tri_r - 0.5)
-        CGPathAddLineToPoint(trianglePipePath, nil, pipeWidth * (1/2), 0)
-        CGPathAddArc        (trianglePipePath, nil, 0, 0, pipeWidth * (1/2), 0, CGFloat(M_PI), true)
-        CGPathCloseSubpath  (trianglePipePath)
-        transformScale = CGAffineTransformMakeScale(1.0, 0.95)
-        trianglePipePath = CGPathCreateMutableCopyByTransformingPath(trianglePipePath, &transformScale)!
-        self.pipePaths[.TriangleUp] = trianglePipePath
-        self.pipePaths[.TriangleDown] = trianglePipePath
-        self.pipePaths[.TriangleLeft] = trianglePipePath
-        self.pipePaths[.TriangleRight] = trianglePipePath
+        totalHeight = CGFloat(self.logicalBoardHeight) * (triangleUp.r + triangleUp.R)
         
     }
     
@@ -105,36 +68,36 @@ class SquareTriangleCrazyScene: AbstractGameBoardScene {
         // Starting points
         switch piece.type {
         case .Square30:
-            x = (squareWidth * 0.5) + tri_r + tri_R
-            y = tri_r
-            x = x + (cos(CGFloat(150.0.degrees)) * (tri_r + (squareWidth*0.5)))
-            y = y + (sin(CGFloat(150.0.degrees)) * (tri_r + (squareWidth*0.5)))
+            x = (square.width * 0.5) + triangleUp.r + triangleUp.R
+            y = triangleUp.r
+            x = x + (cos(CGFloat(150.0.degrees)) * (triangleUp.r + (square.width*0.5)))
+            y = y + (sin(CGFloat(150.0.degrees)) * (triangleUp.r + (square.width*0.5)))
         case .Square:
-            x = squareWidth + tri_r + tri_R
-            y = (squareWidth * 0.5) + (tri_r + tri_R)
+            x = square.width + triangleUp.r + triangleUp.R
+            y = (square.width * 0.5) + (triangleUp.r + triangleUp.R)
         case .TriangleUp:
-            x = squareWidth + tri_r + tri_R
-            y = tri_R
+            x = square.width + triangleUp.r + triangleUp.R
+            y = triangleUp.R
         case .TriangleDown:
-            x = (squareWidth * 0.5) + tri_r + tri_R
-            y = tri_r
+            x = (square.width * 0.5) + triangleUp.r + triangleUp.R
+            y = triangleUp.r
         case .TriangleLeft:
-            x = (squareWidth * 0.5) + tri_R
-            y = (tri_r + tri_R) + (squareWidth * 0.5)
+            x = (square.width * 0.5) + triangleUp.R
+            y = (triangleUp.r + triangleUp.R) + (square.width * 0.5)
         case .TriangleRight:
-            x = tri_r + tri_R + (squareWidth * 1.5) + tri_r
-            y = tri_r + tri_R + (squareWidth * 0.5)
+            x = triangleUp.r + triangleUp.R + (square.width * 1.5) + triangleUp.r
+            y = triangleUp.r + triangleUp.R + (square.width * 0.5)
         default:
             x = 0
             y = 0
         }
         
         // Move them steps up and left
-        x = x + (squareWidth * 0.5 * CGFloat(stepsUp))
-        y = y + ((squareWidth + tri_r + tri_R) * CGFloat(stepsUp))
+        x = x + (square.width * 0.5 * CGFloat(stepsUp))
+        y = y + ((square.width + triangleUp.r + triangleUp.R) * CGFloat(stepsUp))
         
-        x = x + ((squareWidth + tri_r + tri_R) * CGFloat(stepsLeft))
-        y = y + (squareWidth * -0.5 * CGFloat(stepsLeft))
+        x = x + ((square.width + triangleUp.r + triangleUp.R) * CGFloat(stepsLeft))
+        y = y + (square.width * -0.5 * CGFloat(stepsLeft))
 
 
         return CGPoint(x: x, y: size.height - y - (size.height - totalHeight)/2)
